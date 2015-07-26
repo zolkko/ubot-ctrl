4168c3cf3dde5dac4f62ddcbfaaf378164433cac
zolkko <zolkko@gmail.com>
2015-07-26 14:43:57 +0300

    threading test

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 
 mkdir build
 cd build
-cmake ../ -DCMAKE_TOOLCHAIN_FILE=../stm32f407_gcc.cmake -DCMAKE_BUILD_TYPE=Release
+cmake ../ -DCMAKE_TOOLCHAIN_FILE=../stm32f407_gcc.cmake -DCMAKE_BUILD_TYPE=Debug
 make
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,55 @@
+
+#include <stdint.h>
 #include "mbed.h"
 #include "Thread.h"
 
-
-void some_thread(void const *args)
+uint16_t do_work(uint16_t value)
 {
-    int i, j = 0;
+    return value + 1;
+}
+
+
+void thread1_func(void const *args)
+{
+    uint16_t i = 0;
 
     while (true) {
-        for (i = 0; i < 100; i++) {
-            j += i + 3;
+        if (i > 100) {
+            i = 0;
+        } else {
+            i = do_work(i);
         }
-        i = j;
+
+        rtos::Thread::yield();
+    }
+}
+
+
+void thread2_func(void const *args)
+{
+    uint16_t j = 0;
+    while (true) {
+        if (j > 100) {
+            j = 0;
+        } else {
+            j = do_work(j);
+        }
+
+        rtos::Thread::yield();
     }
 }
 
 
 int main()
 {
-    rtos::Thread thread(some_thread);
+    rtos::Thread thread1(thread1_func);
+    rtos::Thread thread2(thread2_func);
 
     while (true) {
     }
 
-    thread.terminate();
+    thread2.terminate();
+    thread1.terminate();
 
     return 0;
 }
